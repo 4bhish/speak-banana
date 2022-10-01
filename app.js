62012e78d38c734d07e8aa4e1bdecd44e0e52df7
4bhish <108976136+4bhish@users.noreply.github.com>
2022-10-01 23:24:44 +0530

    Update Style.css
    styled some key builds in style.css

--- app.js
+++ app.js
@@ -1,4 +1,27 @@
 const btnTranslate = document.querySelector("#translate-button");
+const inputTxt = document.querySelector("#input-txt");
+const outputDiv = document.querySelector("#output-div");
+
+var url="https://api.funtranslations.com/translate/minion.json";
+
+function constructUrl(text)
+{
+    return url + "?" + "text=" + inputTxt.value;
+}
+
+
+
+
+
 btnTranslate.addEventListener("click", function clickHandler(){
-    console.log("clicked");
+
+    var txtInput = inputTxt.value;
+    
+    fetch(constructUrl (txtInput))
+    .then(response => response.json())
+    .then(json => {
+        var finalTranslation = json.contents.translated;
+        outputDiv.innerText = finalTranslation;
+    })
 })
+
